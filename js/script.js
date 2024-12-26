ba9b785d375b2f457a4eba2e12504ef5b0de0b91
Atharva Pandey <93546392+Finite-Code@users.noreply.github.com>
2024-12-26 23:22:38 +0530

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Load Navbar and Footer into every page
+document.getElementById("navbar").innerHTML = `
+  <nav>
+    <a href="index.html">Home</a>
+    <a href="features.html">Features</a>
+    <a href="pro.html">Pro Version</a>
+    <a href="contact.html">Contact</a>
+  </nav>
+`;
+
+document.getElementById("footer").innerHTML = `
+  <footer>
+    <p>&copy; 2024 DeskThing. All Rights Reserved.</p>
+  </footer>
+`;
